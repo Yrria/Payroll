d6bec4e0d6004a7e634600fa6b5c8d3122ad8a9d
Rodney22-blimp <ic.rodney.santos@cvsu.edu.ph>
2024-12-19 17:55:04 +0800

    --LEAVE--

--- payroll/adminpage/leave_approved.php
+++ payroll/adminpage/leave_approved.php
@@ -25,15 +25,92 @@
                 <hr>
             </div>
             <div class="content">
-                <p>This is a simple responsive page with a header and a side navigation bar that can be minimized.</p>
-                <br>
-                <p>This is a simple responsive page with a header and a side navigation bar that can be minimized.</p>
-                <br>
-                <p>This is a simple responsive page with a header and a side navigation bar that can be minimized.</p>
-                <br>
-                <p>This is a simple responsive page with a header and a side navigation bar that can be minimized.</p>
-                <br>
-                <p>This is a simple responsive page with a header and a side navigation bar that can be minimized.</p>
+                <div class="search">
+                    <h3>Approved</h3>
+                    <div class="search-bar">
+                        <button class="view-btn">Search</button>
+                        <input type="text" placeholder="Search employee..." />
+                    </div>
+                </div>
+
+                <!-- Approved Table -->
+                <div class="table-container">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Employee ID</th>
+                                <th>Subject</th>
+                                <th>Name</th>
+                                <th>Date</th>
+                                <th>Leave Type</th>
+                                <th>Message</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Emp 000</td>
+                                <td>Manager</td>
+                                <td>Willy Wonka</td>
+                                <td>01-06-2024</td>
+                                <td>Emergency Leave</td>
+                                <td>I am feeling unwell and have been advised by my doctor to rest and recover at home
+                                </td>
+                                <td><button class="view-btn">View Info</button></td>
+                            </tr>
+                            <tr>
+                                <td>Emp 000</td>
+                                <td>Crew</td>
+                                <td>Ken Flerlage</td>
+                                <td>01-06-2024</td>
+                                <td>Sick Leave</td>
+                                <td>I am feeling unwell and have been advised by my doctor to rest and recover at home
+                                </td>
+                                <td><button class="view-btn">View Info</button></td>
+                            </tr>
+                            <tr>
+                                <td>Emp 000</td>
+                                <td>Crew</td>
+                                <td>George Orwell</td>
+                                <td>01-06-2024</td>
+                                <td>Birthday Leave</td>
+                                <td>I am feeling unwell and have been advised by my doctor to rest and recover at home
+                                </td>
+                                <td><button class="view-btn">View Info</button></td>
+                            </tr>
+                            <tr>
+                                <td>Emp 000</td>
+                                <td>Crew</td>
+                                <td>Natalie Maines</td>
+                                <td>01-06-2024</td>
+                                <td>Maternity Leave</td>
+                                <td>I am feeling unwell and have been advised by my doctor to rest and recover at home
+                                </td>
+                                <td><button class="view-btn">View Info</button></td>
+                            </tr>
+                            <tr>
+                                <td>Emp 000</td>
+                                <td>Crew</td>
+                                <td>Hellen Mirren</td>
+                                <td>01-06-2024</td>
+                                <td>Vacation Leave</td>
+                                <td>I am feeling unwell and have been advised by my doctor to rest and recover at home
+                                </td>
+                                <td><button class="view-btn">View Info</button></td>
+                            </tr>
+                        </tbody>
+                    </table>
+
+                    <!-- Pagination -->
+                    <div class="pagination">
+                        <p>Showing 1 / 100 Results</p>
+                        <div class="pagination">
+                            <button>Prev</button>
+                            <input type="text" value="1" readonly />
+                            <button>Next</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
--- payroll/adminpage/leave_pending.php
+++ payroll/adminpage/leave_pending.php
@@ -7,7 +7,7 @@
     <link rel="shortcut icon" href="../assets/logo.png" type="image/svg+xml">
     <link rel="stylesheet" href="./css/main.css">
     <link rel="stylesheet" href="./css/leave.css">
-    <title>Leave - Approved</title>
+    <title>Leave - Pending</title>
 </head>
 
 <body>
@@ -25,15 +25,120 @@
                 <hr>
             </div>
             <div class="content">
-                <p>This is a simple responsive page with a header and a side navigation bar that can be minimized.</p>
-                <br>
-                <p>This is a simple responsive page with a header and a side navigation bar that can be minimized.</p>
-                <br>
-                <p>This is a simple responsive page with a header and a side navigation bar that can be minimized.</p>
-                <br>
-                <p>This is a simple responsive page with a header and a side navigation bar that can be minimized.</p>
-                <br>
-                <p>This is a simple responsive page with a header and a side navigation bar that can be minimized.</p>
+                <div class="search">
+                    <h3>Pending</h3>
+                    <div class="search-bar">
+                        <button class="view-btn">Search</button>
+                        <input type="text" placeholder="Search employee..." />
+                    </div>
+                </div>
+
+                <!-- Leave Table -->
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Employee ID</th>
+                            <th>Subject</th>
+                            <th>Name</th>
+                            <th>Date</th>
+                            <th>Leave Type</th>
+                            <th>Message</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>001</td>
+                            <td>Manager</td>
+                            <td>Willy Wonka</td>
+                            <td>01-06-24</td>
+                            <td>Emergency Leave</td>
+                            <td class="td-text">I am feeling unwell and have been advised by my doctor to rest and recover at home
+                            </td>
+                            <td class="action-tb">
+                                <div class="action-buttons">
+                                    <button class="view-btn">View Info</button>
+                                    <button class="view-btn">Approve</button>
+                                    <button class="view-btn">Decline</button>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>002</td>
+                            <td>Crew</td>
+                            <td>Ken Flerlage</td>
+                            <td>01-06-24</td>
+                            <td>Annual Leave</td>
+                            <td class="td-text">I am feeling unwell and have been advised by my doctor to rest and recover at home
+                            </td>
+                            <td class="action-tb">
+                                <div class="action-buttons">
+                                    <button class="view-btn">View Info</button>
+                                    <button class="view-btn">Approve</button>
+                                    <button class="view-btn">Decline</button>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>003</td>
+                            <td>Crew</td>
+                            <td>George Orwell</td>
+                            <td>01-06-24</td>
+                            <td>Bereavement Leave</td>
+                            <td class="td-text">I am feeling unwell and have been advised by my doctor to rest and recover at home
+                            </td>
+                            <td class="action-tb">
+                                <div class="action-buttons">
+                                    <button class="view-btn">View Info</button>
+                                    <button class="view-btn">Approve</button>
+                                    <button class="view-btn">Decline</button>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>004</td>
+                            <td>Crew</td>
+                            <td>Natalie Maines</td>
+                            <td>01-06-24</td>
+                            <td>Maternity Leave</td>
+                            <td class="td-text">I am feeling unwell and have been advised by my doctor to rest and recover at home
+                            </td>
+                            <td class="action-tb">
+                                <div class="action-buttons">
+                                    <button class="view-btn">View Info</button>
+                                    <button class="view-btn">Approve</button>
+                                    <button class="view-btn">Decline</button>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>005</td>
+                            <td>Crew</td>
+                            <td>Hellen Mirren</td>
+                            <td>01-06-24</td>
+                            <td>Vacation Leave</td>
+                            <td class="td-text">I am feeling unwell and have been advised by my doctor to rest and recover at home
+                            </td>
+                            <td class="action-tb">
+                                <div class="action-buttons">
+                                    <button class="view-btn">View Info</button>
+                                    <button class="view-btn">Approve</button>
+                                    <button class="view-btn">Decline</button>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <br></br>
+                <!-- Pagination -->
+                <div class="pagination">
+                    <p>Showing 1 / 100 Results</p>
+                    <div class="pagination">
+                        <button>Prev</button>
+                        <input type="text" value="1" readonly />
+                        <button>Next</button>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
