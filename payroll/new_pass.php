4e13bcd2c5acc7139dfea060f1e71a97b74762a9
Illyasviel-Von-Einzbern <fidelisalen@email.com>
2024-12-25 21:16:45 +0800

    login, Forgot-ass, ott, new-ass

--- payroll/new_pass.php
+++ payroll/new_pass.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Login</title>
-    <link rel="stylesheet" href="./assets//css/login.css">
+    <link rel="stylesheet" href="./assets/css/new_pass.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 
@@ -13,26 +13,24 @@
     <div class="login-container">
         <div class="login-box">
         <a href="./index.php" class="title-tags"><span class="comp-name"><img src="./assets/title.png" class="title-name" alt="ExPense"></span></a>
-            <p>Welcome to [Company Name]'s Payroll System! Easily manage your payroll, access pay slips, and keep track of your payment details all in one secure place. Log in with your employee ID and password to get started.</p>
+            <p>To change your password, ensure it’s complex, containing a mix of uppercase letters, lowercase letters, numbers, and special characters.</p>
             <form action="#">
-                <label for="employee-id">Employee ID:</label>
-                <div class="input-container">
-                    <i class="fas fa-user icon"></i>
-                    <input type="text" id="employee-id" placeholder="Enter Employee ID" required>
-                </div>
-
-                <label for="password">Password:</label>
+                <label for="employee-id">Password:</label>
                 <div class="input-container">
                     <i class="fas fa-eye toggle-password" id="toggle-password"></i>
-                    <input type="password" id="password" placeholder="Enter Password" required>
+                    <input type="text" id="employee-id" placeholder="Enter Password" required>
+                </div>
+
+                <label for="password">Confirm Password:</label>
+                <div class="input-container">
+                    <i class="fas fa-eye toggle-password" id="toggle-password"></i>
+                    <input type="password" id="password" placeholder="Re-Enter Password" required>
                 </div>
 
                 <div class="buttons">
-                    <!-- <button type="button" class="forgot-password">Forgot Password</button>
-                        <button type="submit" class="login">Login</button> -->
-
-                    <a href="./forgot_pass.php" class="forgot-password">Forgot Password</a>
-                    <a href="./adminpage/dashboard.php" class="login">Login</a>
+                    <!-- <button type="submit" class="change">Change</button> -->
+                     
+                    <a href="./index.php" class="change">Change</a>
                 </div>
             </form>
         </div>
