cc75636967800442bba1940894e57a3fe00c2a78
6nix <hadesnicol@gmail.com>
2025-01-11 01:58:10 +0800

    reports, updated logo and icon

--- payroll/adminpage/create_payslip.php
+++ payroll/adminpage/create_payslip.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="../assets/logo.png" type="image/svg+xml">
+    <link rel="shortcut icon" href="../assets/logowhite-.png" type="image/svg+xml">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
     <link rel="stylesheet" href="./css/main.css">
     <link rel="stylesheet" href="./css/create_payslip.css">
